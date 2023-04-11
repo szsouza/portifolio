92bc95f68a4faad5e52dab6b53d5d5c478336070
Rodrigo Souza <rodrigosouzaprof.ff@gmail.com>
2023-04-11 10:54:57 -0300

    complete

--- src/components/effects/typingEffect.js
+++ src/components/effects/typingEffect.js
@@ -1,51 +1,34 @@
-import React from "react";
-// import Typed from "typed.js";
-import "../../pages/Home/Home.css"
-import TypeWriterEffect from 'react-typewriter-effect';
 
-// function TypingEffect() {
-
-
-//   React.useEffect(() => {
-//     const typed = new Typed(".multitext",{
-//       strings: ['Rodrigo'],
-//       loop : true,
-//       typeSpeed: 100,
-//       backSpeed: 80,
-//       backDelay: 1500
-//     })
-
-//     return () => {
-//       // Destroy Typed instance during cleanup to stop animation
-//       typed.destroy();
-//     };
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <span className="multitext"></span>
-//     </div>
-//   );
-// }
+import "../../pages/Home/Home.css";
+import Typical from "react-typical";
 
 const TypingEffect = () => {
   return (
-    <TypeWriterEffect
-        textStyle={{
-          fontWeight: 800,
-        }}
-        startDelay={200}
-        cursorColor="#3F3D56"
-        multiText={[
-          'Rodrigo Souza'
-        ]}
-        multiTextDelay={2000}
-        typeSpeed={50}
-        multiTextLoop
-      />
-  )
-}
+    // <TypeWriterEffect
+    //     textStyle={{
+    //       fontWeight: 800,
+    //     }}
+    //     startDelay={200}
+    //     cursorColor="#3F3D56"
+    //     multiText={[
+    //       "Rodrigo Souza", 
+    //     ]}
+    //     multiTextDelay={2000}
+    //     typeSpeed={50}
+    //     multiTextLoop
+    //   />
 
-export default TypingEffect
+    <Typical
+            loop={Infinity}
+            steps={[
+              "Rodrigo Souza",
+              1000,
+              "",
+              1000
+              
+            ]}
+          />
+      
+      )}
 
-
+export default TypingEffect;
